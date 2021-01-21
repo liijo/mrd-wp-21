c1d843646b0e7402d6e8c0e9c9b97ee6426c2662
lijo-mrdigital <lijo@mr-digital.co.uk>
2021-01-21 13:41:31 +0530

    Strategy session update

--- wp-content/themes/mr-digital-21/parts/template-strategy-session.php
+++ wp-content/themes/mr-digital-21/parts/template-strategy-session.php
@@ -1,18 +1,18 @@
-<?php $resultId = get_theme_mod('strategy_session'); 
-if(!empty($resultId)) {?>
+<?php $pageId = get_the_id(); 
+if(!empty($pageId)) {?>
 
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <div class="text-center mb-5">
-                    <?php if( ! empty (get_field('session_title', $resultId)) ){?>
+                    <?php if( ! empty (get_field('session_title', $pageId)) ){?>
                     <h3 class="section-title mb-3">
-                        <?php echo get_field('session_title', $resultId);?>
+                        <?php echo get_field('session_title', $pageId);?>
                     </h3>
                     <?php } ?>
                 </div>
-                <?php if( ! empty (get_field('session_content', $resultId)) )
-                    echo get_field('session_content', $resultId);?>
+                <?php if( ! empty (get_field('session_content', $pageId)) )
+                    echo get_field('session_content', $pageId);?>
             </div>
         </div>
     </div>
